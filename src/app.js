36d270f1c46850c4a8e6400e05161c877579bce8
aniketsharmaa <aniket.raj.9753@gmail.com>
2024-04-05 02:03:20 +0530

    changed

--- src/app.js
+++ src/app.js
@@ -1,9 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Header } from "./components/Header"
-import SearchBar from "./components/SearchBar";
-import ResturantCard from "./components/ResturantCard";
-import { resList } from "./utils/mockData";
 import Body from "./components/Body";
 
 
