125388bb8cab895a0d510a7be0c03a4687ab5068
aniketsharmaa <aniket.raj.9753@gmail.com>
2024-04-13 23:01:10 +0530

    fetch

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,21 +1,38 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import SearchBar from "./SearchBar";
 import ResturantCard from "./ResturantCard";
 import { resList } from "../utils/mockData";
+import { swiggyApi } from "../utils/constant";
+import axios from "axios";
 
-var apiUrl = "https://www.swiggy.com/dapi/restaurants/list/v5?lat=25.5940947&lng=85.1375645&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
 
-async function fetchData(){
-    var data = fetch(apiUrl);
-    data = (await data).json
-    console.log(data)
-}
-fetchData();
+
+
+
 
 
 const Body = () => {
     let listOfResturant = resList
     const [list, setList] = useState(listOfResturant)
+    const fetchData = async () => {
+        try {
+            const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=25.5940947&lng=85.1375645&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING")
+            const json = await data.json()
+            console.log(json)
+    
+        } catch (error) {
+            console.log('this is error',error)
+        }
+
+
+    }
+    useEffect(() => {
+        console.log("use effect is called: ")
+        fetchData()
+
+
+    }, [])
+
 
     return (
 
@@ -23,7 +40,7 @@ const Body = () => {
             <div className="filter">
                 <button className="filter-btn" onClick={() => {
                     const filteredList = list.filter((it) => it.data.avgRating > 4.3
-                     );
+                    );
                     setList([...filteredList]);
                     console.log("done")
                 }}>
--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,4 +1,6 @@
-export const Img_logo="https://img.freepik.com/free-vector/hand-drawn-pizza-delivery-man-with-scooter_23-2147674445.jpg?w=740&t=st=1711838469~exp=1711839069~hmac=af757b5f613a8359343c5391dedb1c601c39cece510dcaaf1f201bc4dc9f97ae";
+export const Img_logo = "https://img.freepik.com/free-vector/hand-drawn-pizza-delivery-man-with-scooter_23-2147674445.jpg?w=740&t=st=1711838469~exp=1711839069~hmac=af757b5f613a8359343c5391dedb1c601c39cece510dcaaf1f201bc4dc9f97ae";
 
 
 export const Res_card_img = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
+
+export const swiggyApi = "https://www.swiggy.com/dapi/restaurants/list/v5?lat=25.5940947&lng=85.1375645&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
